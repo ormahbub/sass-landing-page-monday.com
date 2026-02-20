76a6adfc8b5afb14233a72f4ccaf180f1febe7e5
Mahbub <72622711+ormahbub@users.noreply.github.com>
2026-02-20 15:18:43 +0600

    Header design updated using tailwind css.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import productSwitcherIcon from "../../assets/images/product-switcher-icon.png";
 
 const Header = () => {
   return (
-    <header id="header" className="site-header">
+    <header id="header" className="site-header py-4">
       <div className="container flex justify-between items-center">
         <div className="header-left flex items-center gap-5">
           <div className="site-logo max-w-[168px]">
             <img className="max-w-full" src={mondayLogo} alt="Monday Logo" />
           </div>
           <nav className="nav-menu">
-            <ul className="primary-menu flex gap-5">
+            <ul className="primary-menu flex gap-5 text-sm text-[var(--text-color)]">
               <li className="menu-item">
                 <a href="#">Products</a>
               </li>
@@ -32,7 +32,7 @@ const Header = () => {
         </div>
         <div className="header-right flex items-center gap-5">
           <nav className="nav-menu">
-            <ul className="secondary-menu flex gap-5">
+            <ul className="secondary-menu flex gap-5 text-sm text-[var(--text-color)]">
               <li className="menu-item">
                 <a href="#">Pricing</a>
               </li>
@@ -40,19 +40,26 @@ const Header = () => {
                 <a href="#">Log in</a>
               </li>
               <li className="menu-item">
-                <a className="btn header-btn-1" href="#">
+                <a
+                  className="btn header-btn-1 border border-[var(--primary-color)] text-[var(--primary-color)] py-2 px-4 rounded-4xl"
+                  href="#"
+                >
                   Contact sales
                 </a>
               </li>
               <li className="menu-item">
-                <a className="btn header-btn-2" href="#">
+                <a className="btn header-btn-2 py-2 px-4 border border-[var(--primary-color)] bg-[var(--primary-color)] text-white rounded-4xl" href="#">
                   Get Started
                 </a>
               </li>
             </ul>
           </nav>
-          <button className="product-switcher-btn">
-            <img src={productSwitcherIcon} alt="Product Switcher Button" />
+          <button className="product-switcher-btn max-w-[40px]">
+            <img
+              className="max-w-full"
+              src={productSwitcherIcon}
+              alt="Product Switcher Button"
+            />
           </button>
         </div>
       </div>
