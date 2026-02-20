fb4b971361b7c01075d6cf9f8d3e6306435a0db5
Mahbub <72622711+ormahbub@users.noreply.github.com>
2026-02-20 16:58:36 +0600

    NavMenu now work by pulling data from json file.

--- src/components/Elements/NavMenu.jsx
+++ src/components/Elements/NavMenu.jsx
@@ -1,24 +1,18 @@
 import NavLink from "./NavLink";
+import menuData from "../../data/menu.json";
 
 const NavMenu = () => {
   return (
     <nav className="nav-menu">
       <ul className="primary-menu flex gap-5 text-sm text-[var(--text-color)]">
-        <li className="menu-item">
-          <NavLink text={"Products"} url={"#"} dropdown={true} />
-        </li>
-        <li className="menu-item">
-          <NavLink text={"AI"} url={"#"} dropdown={true} />
-        </li>
-        <li className="menu-item">
-          <NavLink text={"Solutions"} url={"#"} dropdown={true} />
-        </li>
-        <li className="menu-item">
-          <NavLink text={"Resources"} url={"#"} dropdown={true} />
-        </li>
-        <li className="menu-item">
-          <NavLink text={"Enterprise"} url={"#"} dropdown={false} />
-        </li>
+        {menuData[0].menuItems.map((menuItem, index) => {
+          const { itemName, itemUrl, dropdown } = menuItem;
+          return (
+            <li key={index}>
+              <NavLink text={itemName} url={itemUrl} dropdown={dropdown} />
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
