0e3c5b548afee12aaef064c3d8875039523edcb0
Mahbub <72622711+ormahbub@users.noreply.github.com>
2026-03-01 21:04:46 +0600

    checkbox selection is working.

--- src/components/Elements/ProductSelectionItem.jsx
+++ src/components/Elements/ProductSelectionItem.jsx
@@ -1,8 +1,20 @@
-const ProductSelectionItem = ({ className, productName }) => {
+const ProductSelectionItem = ({ className, productName, idNum }) => {
   return (
-    <div className={`${className} w-[31%]`}>
-      <input type="checkbox" />
-      <span>{productName}</span>
+    <div
+      className={`${className} w-[32.5%] h-[120px] border-[1px] rounded-[4px] border-[#dddddd]`}
+    >
+      <label
+        className="flex flex-col justify-center items-center gap-y-2 h-full w-full"
+        htmlFor={`checkbox_000${idNum}`}
+      >
+        <input
+          className="h-[20px] w-[20px]"
+          id={`checkbox_000${idNum}`}
+          name={`checkbox_000${idNum}`}
+          type="checkbox"
+        />
+        <span className="text-[13px]">{productName}</span>
+      </label>
     </div>
   );
 };
--- src/components/ProductSelectionArea.jsx
+++ src/components/ProductSelectionArea.jsx
@@ -15,42 +15,51 @@ const ProductSelectionArea = () => {
         />
       </div>
       <div className="product-selection-gird absolute top-[0]">
-        <div className="checkbox-gird flex justify-space-between items-center flex-wrap bg-[var(--bg-color)] w-[400px]">
+        <div className="checkbox-gird flex justify-between items-center flex-wrap bg-[var(--bg-color)] w-[400px] gap-y-1 p-4 rounded-xl shadow-xl">
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={1}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={2}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={3}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={4}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={5}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={6}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={7}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={8}
           />
           <ProductSelectionItem
             className={"product-and-takks"}
             productName={"Product & Tasks"}
+            idNum={9}
           />
         </div>
       </div>
